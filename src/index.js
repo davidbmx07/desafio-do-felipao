4561f156652eddf7fb138bcfd0fba66ff602b587
david-dev <david-bmx32@outlook.com>
2023-12-01 14:39:49 -0300

    meu segundo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-let nome = "david";
-console.log(nome)
+let nomeHeroi = "Garen"
+let xpHeroi = 2546
+
+if(xpHeroi < 1000){
+    nivel = "Ferro"
+}else if(xpHeroi > 1001 && xpHeroi < 2000){
+    nivel - "Bronze"
+}else if(xpHeroi > 2001 && xpHeroi < 5000){
+    nivel = "Prata"
+}else if(xpHeroi > 5001 && xpHeroi < 7000){
+    nivel - "Ouro"
+}else if(xpHeroi > 7001 && xpHeroi < 8000){
+    nivel = "Platina"
+}else if(xpHeroi > 8001 && xpHeroi < 9000){
+    nivel - "Diamante"
+}else if(xpHeroi > 9001 && xpHeroi < 10000){
+    nivel = "Mestre"
+}else if(xpHeroi > 10001){
+    nivel - "Challenger"
+}
+
+console.log("O Herói " + nomeHeroi + " está no nível XP = " + nivel)
